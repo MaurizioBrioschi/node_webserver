c2290d8e1f054d55da3b01b27b1f93a7000cb238
ridesoft <maurizio.brioschi@ridesoft.org>
2015-04-14 10:51:09 +0200

    add routes

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
 var http = require('http');
+var path = require('path');
+
+var pages = [{
+  route: '',
+  output: 'rock it!'
+}, {
+  route: 'about',
+  output: 'I\'m a rock\'n roll star'
+}, {
+  route: 'skate',
+  output: function() {
+    return 'skate it!\n' + this.route;
+  }
+}, ];
+
 http.createServer(function(request, response) {
-  response.writeHead(200, {
-    'Content-Type': 'text/html'
+  var lookup = path.basename(decodeURI(request.url));
+  pages.forEach(function(page) {
+    if (page.route === lookup) {
+      response.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+      response.end(typeof page.output === 'function' ? page.output() : page.output);
+    }
   });
-  response.end('Woohoo!');
+  if (!response.finished) {
+    response.writeHead(404);
+    response.end('Page Not Found!');
+  }
 }).listen(8069);
