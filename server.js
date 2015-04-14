bb63a4cab844978cfb7c6d9b674212239ee50b15
ridesoft <maurizio.brioschi@ridesoft.org>
2015-04-14 12:40:46 +0200

    part to read external html file if passedin the url

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var path = require('path');
+var fs = require('fs');
 
 var pages = [{
   route: '/',
@@ -7,18 +8,28 @@ var pages = [{
 }, {
   route: '/about',
   output: 'I\'m a rock\'n roll star'
-},{
+}, {
   route: '/snowboard/trick',
   output: 'my snowboard tricks'
-} ,{
+}, {
   route: '/skate',
   output: function() {
     return 'skate it!\n' + this.route;
   }
 }, ];
 
+var mimeTypes = {
+  '.js': 'text/javascript',
+  '.html': 'text/html',
+  '.css': 'text/css'
+};
+
+/**
+here the script to create the server
+**/
 http.createServer(function(request, response) {
-  var lookup = decodeURI(request.url);
+  var lookup = decodeURI(request.url) || 'master.html';
+
   pages.forEach(function(page) {
     if (page.route === lookup) {
       response.writeHead(200, {
@@ -27,8 +38,24 @@ http.createServer(function(request, response) {
       response.end(typeof page.output === 'function' ? page.output() : page.output);
     }
   });
-  if (!response.finished) {
-    response.writeHead(404);
-    response.end('Page Not Found!');
-  }
+  var f = 'content/' + lookup;
+  fs.exists(f, function(exists) {
+    if (exists) {
+      fs.readFile(f, function(err, data) {
+        if (err) {
+          response.writeHead(500);
+          response.end('Server Error!');
+          return;
+        }
+        var headers = {
+          'Content-type': mimeTypes[path.extname(lookup)]
+        };
+        response.writeHead(200, headers);
+        response.end(data);
+      });
+      return;
+    }
+    response.writeHead(404); //no such file found!
+    response.end();
+  });
 }).listen(8069);
