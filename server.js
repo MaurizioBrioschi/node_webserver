b63d5729ea2fe5614a776f1eb09449646dbe9ee0
ridesoft <maurizio.brioschi@ridesoft.org>
2015-04-14 12:01:02 +0200

    fix, add decodeURI functionto permit a multilevel route

--- server.js
+++ server.js
@@ -2,20 +2,23 @@ var http = require('http');
 var path = require('path');
 
 var pages = [{
-  route: '',
+  route: '/',
   output: 'rock it!'
 }, {
-  route: 'about',
+  route: '/about',
   output: 'I\'m a rock\'n roll star'
-}, {
-  route: 'skate',
+},{
+  route: '/snowboard/trick',
+  output: 'my snowboard tricks'
+} ,{
+  route: '/skate',
   output: function() {
     return 'skate it!\n' + this.route;
   }
 }, ];
 
 http.createServer(function(request, response) {
-  var lookup = path.basename(decodeURI(request.url));
+  var lookup = decodeURI(request.url);
   pages.forEach(function(page) {
     if (page.route === lookup) {
       response.writeHead(200, {
